dcf6c1f6b0439cdca5ce7a57b45f7f95017a6277
Javier Francisco Cuadra <32556567+javiercuadra@users.noreply.github.com>
2024-03-05 14:15:23 -0600

    use closures to prevent function redeclaration in package

--- static_analyser/pkg/parser/FindRegisterInstanceWrapperInvocations.go
+++ static_analyser/pkg/parser/FindRegisterInstanceWrapperInvocations.go
@@ -7,6 +7,23 @@ import (
 	"strings"
 )
 
+func _handleBasicLit(arg ast.Expr) string {
+	if lit, ok := arg.(*ast.BasicLit); ok {
+		return lit.Value
+	}
+	return "nil"
+}
+
+func resolveArgument(arg interface{}, args []string) string {
+	switch t := arg.(type) {
+	case string:
+		return t
+	case t.WrapperParams:
+		return strings.ReplaceAll(args[t.Position], "\"", "")
+	}
+	return ""
+}
+
 // finds the invocation of the wrappers for register instance and resolves the arguments for serviceName, Ip, and Port
 func FindRegisterInstanceWrapperInvocations(node ast.Node, wrapper t.RegisterInstanceWrapper, service string) ([]string, []t.ServiceInfo) {
 	wrapperName := wrapper.Wrapper
@@ -18,56 +35,91 @@ func FindRegisterInstanceWrapperInvocations(node ast.Node, wrapper t.RegisterIns
 		}
 
 		switch n := n.(type) {
-
 		case *ast.CallExpr:
-			// log.Printf("Call expression: %s\n", n.Fun)
 			if fun, ok := n.Fun.(*ast.Ident); ok {
 				var args []string
 				if fun.Name == wrapperName {
-
 					for _, arg := range n.Args {
-
-						if lit, ok := arg.(*ast.BasicLit); ok {
-							args = append(args, lit.Value)
-						} else {
-							args = append(args, "nil")
-						}
+						args = append(args, _handleBasicLit(arg))
 					}
 					fmt.Printf("%v", args)
 
-					serviceName := ""
-					ip := ""
-					port := ""
+					serviceName := resolveArgument(wrapper.ServiceName, args)
+					ip := resolveArgument(wrapper.IP, args)
+					port := resolveArgument(wrapper.Port, args)
 
-					switch t := wrapper.ServiceName.(type) {
-					case string:
-						serviceName = t
-					case t.WrapperParams:
-						serviceName = strings.ReplaceAll(args[t.Position], "\"", "")
-					}
-					switch t := wrapper.IP.(type) {
-					case string:
-						ip = t
-					case t.WrapperParams:
-						ip = strings.ReplaceAll(args[t.Position], "\"", "")
-					}
-					switch t := wrapper.Port.(type) {
-					case string:
-						port = t
-					case t.WrapperParams:
-						port = strings.ReplaceAll(args[t.Position], "\"", "")
-					}
 					serviceInfos = append(serviceInfos, t.ServiceInfo{Application: service, IP: ip, Port: port})
 					serviceNames = append(serviceNames, serviceName)
 				}
-
 			}
-			// var instances [].RegisterInfo
-
 		}
-		// return true
 		return true
 	})
 
 	return serviceNames, serviceInfos
 }
+
+// // finds the invocation of the wrappers for register instance and resolves the arguments for serviceName, Ip, and Port
+// func FindRegisterInstanceWrapperInvocations(node ast.Node, wrapper t.RegisterInstanceWrapper, service string) ([]string, []t.ServiceInfo) {
+// 	wrapperName := wrapper.Wrapper
+// 	serviceNames := []string{}
+// 	serviceInfos := []t.ServiceInfo{}
+// 	ast.Inspect(node, func(n ast.Node) bool {
+// 		if n == nil {
+// 			return false
+// 		}
+
+// 		switch n := n.(type) {
+
+// 		case *ast.CallExpr:
+// 			// log.Printf("Call expression: %s\n", n.Fun)
+// 			if fun, ok := n.Fun.(*ast.Ident); ok {
+// 				var args []string
+// 				if fun.Name == wrapperName {
+
+// 					for _, arg := range n.Args {
+
+// 						if lit, ok := arg.(*ast.BasicLit); ok {
+// 							args = append(args, lit.Value)
+// 						} else {
+// 							args = append(args, "nil")
+// 						}
+// 					}
+// 					fmt.Printf("%v", args)
+
+// 					serviceName := ""
+// 					ip := ""
+// 					port := ""
+
+// 					switch t := wrapper.ServiceName.(type) {
+// 					case string:
+// 						serviceName = t
+// 					case t.WrapperParams:
+// 						serviceName = strings.ReplaceAll(args[t.Position], "\"", "")
+// 					}
+// 					switch t := wrapper.IP.(type) {
+// 					case string:
+// 						ip = t
+// 					case t.WrapperParams:
+// 						ip = strings.ReplaceAll(args[t.Position], "\"", "")
+// 					}
+// 					switch t := wrapper.Port.(type) {
+// 					case string:
+// 						port = t
+// 					case t.WrapperParams:
+// 						port = strings.ReplaceAll(args[t.Position], "\"", "")
+// 					}
+// 					serviceInfos = append(serviceInfos, t.ServiceInfo{Application: service, IP: ip, Port: port})
+// 					serviceNames = append(serviceNames, serviceName)
+// 				}
+
+// 			}
+// 			// var instances [].RegisterInfo
+
+// 		}
+// 		// return true
+// 		return true
+// 	})
+
+// 	return serviceNames, serviceInfos
+// }
