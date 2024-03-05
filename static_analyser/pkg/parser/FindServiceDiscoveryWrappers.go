dfbf52f88440c70d8e80a662df5aea4c22355388
Javier Francisco Cuadra <32556567+javiercuadra@users.noreply.github.com>
2024-03-05 14:37:03 -0600

    document and refactor function

--- static_analyser/pkg/parser/FindServiceDiscoveryWrappers.go
+++ static_analyser/pkg/parser/FindServiceDiscoveryWrappers.go
@@ -7,14 +7,168 @@ import (
 	"strings"
 )
 
+// func FindServiceDiscoveryWrappers(node ast.Node) []t.ServiceDiscoveryWrapper {
+// 	// Different nacos SDK functions for service discovery
+// 	select_sdk := []string{"GetService", "SelectAllInstances", "SelectOneHealthyInstance", "SelectInstances", "Subscribe"}
+// 	select_params := []string{"GetServiceParam", "SelectAllInstancesParam", "SelectOneHealthyInstanceParam", "SelectInstancesParam", "SubscribeParam"}
+
+// 	var paramNames = []string{}
+// 	var wrapper string
+// 	var instances []t.ServiceDiscoveryWrapper
+// 	ast.Inspect(node, func(n ast.Node) bool {
+// 		if n == nil {
+// 			return false
+// 		}
+
+// 		switch n := n.(type) {
+// 		case *ast.FuncDecl:
+// 			// Function declaration
+// 			wrapper = n.Name.Name
+// 			for _, param := range n.Type.Params.List {
+// 				for _, name := range param.Names {
+// 					paramNames = append(paramNames, name.Name)
+// 				}
+// 			}
+// 			// log.Printf("Parameter names: %v\n", paramNames)
+
+// 		case *ast.CallExpr:
+// 			// log.Printf("Call expression: %s\n", n.Fun)
+// 			if selExpr, ok := n.Fun.(*ast.SelectorExpr); ok {
+// 				// If the function is a list of nacos sdk functions
+// 				if util.Contains(select_sdk, selExpr.Sel.Name) {
+// 					// log.Printf("%s ", selExpr.Sel.Name)
+// 					for _, arg := range n.Args {
+
+// 						switch arg := arg.(type) {
+// 						case *ast.CompositeLit:
+// 							if sel, ok := arg.Type.(*ast.SelectorExpr); ok {
+
+// 								if util.Contains(select_params, sel.Sel.Name) {
+
+// 									instance := t.ServiceDiscoveryWrapper{}
+// 									instance.Wrapper = wrapper
+// 									for _, elt := range arg.Elts {
+// 										if kv, ok := elt.(*ast.KeyValueExpr); ok {
+// 											if key, ok := kv.Key.(*ast.Ident); ok {
+// 												if key.Name == "ServiceName" {
+// 													switch v := kv.Value.(type) {
+// 													case *ast.Ident:
+// 														for i, paramName := range paramNames {
+// 															if paramName == strings.TrimSpace(v.Name) {
+// 																instance.ServiceName = t.WrapperParams{Position: i}
+// 															}
+// 															if instance.ServiceName == nil {
+// 																instance.ServiceName = util.FindConstValue(node, strings.TrimSpace(v.Name), wrapper)
+// 															}
+// 														}
+// 													case *ast.BasicLit:
+// 														instance.ServiceName = strings.ReplaceAll(strings.TrimSpace(v.Value), "\"", "")
+// 													}
+// 												}
+// 											}
+// 										}
+// 									}
+
+// 									instances = append(instances, instance)
+
+// 								}
+// 							}
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+// 		return true
+// 	})
+// 	return instances
+// }
+
 func FindServiceDiscoveryWrappers(node ast.Node) []t.ServiceDiscoveryWrapper {
-	// Different nacos SDK functions for service discovery
+	// FindServiceDiscoveryWrappers traverses the AST (Abstract Syntax Tree) to find all instances of service discovery calls.
+	//
+	// node: The root node of the AST.
+	//
+	// Returns:
+	// A slice of ServiceDiscoveryWrapper structs. Each struct represents a service discovery call found in the AST.
+	// The ServiceDiscoveryWrapper struct contains the name of the wrapper function and the parameters passed to the service discovery call.
+
 	select_sdk := []string{"GetService", "SelectAllInstances", "SelectOneHealthyInstance", "SelectInstances", "Subscribe"}
 	select_params := []string{"GetServiceParam", "SelectAllInstancesParam", "SelectOneHealthyInstanceParam", "SelectInstancesParam", "SubscribeParam"}
 
 	var paramNames = []string{}
 	var wrapper string
 	var instances []t.ServiceDiscoveryWrapper
+
+	handleFuncDecl := func(n *ast.FuncDecl) {
+		// handleFuncDecl is a closure that handles function declarations.
+		//
+		// n: The function declaration node.
+		//
+		// This closure sets the wrapper variable to the name of the function and adds the names of the parameters to the paramNames slice.
+
+		wrapper = n.Name.Name
+		for _, param := range n.Type.Params.List {
+			for _, name := range param.Names {
+				paramNames = append(paramNames, name.Name)
+			}
+		}
+	}
+
+	handleCallExpr := func(n *ast.CallExpr) {
+		// handleCallExpr is a closure that handles call expressions.
+		//
+		// n: The call expression node.
+		//
+		// This closure checks if the call expression matches the criteria defined by select_sdk and select_params.
+		// If it does, a new ServiceDiscoveryWrapper instance is created and added to the instances slice.
+
+		selExpr, ok := n.Fun.(*ast.SelectorExpr)
+		if !ok || !util.Contains(select_sdk, selExpr.Sel.Name) {
+			return
+		}
+
+		for _, arg := range n.Args {
+			arg, ok := arg.(*ast.CompositeLit)
+			if !ok {
+				continue
+			}
+
+			sel, ok := arg.Type.(*ast.SelectorExpr)
+			if !ok || !util.Contains(select_params, sel.Sel.Name) {
+				continue
+			}
+
+			instance := t.ServiceDiscoveryWrapper{Wrapper: wrapper}
+			for _, elt := range arg.Elts {
+				kv, ok := elt.(*ast.KeyValueExpr)
+				if !ok {
+					continue
+				}
+
+				key, ok := kv.Key.(*ast.Ident)
+				if !ok || key.Name != "ServiceName" {
+					continue
+				}
+
+				switch v := kv.Value.(type) {
+				case *ast.Ident:
+					for i, paramName := range paramNames {
+						if paramName == strings.TrimSpace(v.Name) {
+							instance.ServiceName = t.WrapperParams{Position: i}
+						}
+						if instance.ServiceName == nil {
+							instance.ServiceName = util.FindConstValue(node, strings.TrimSpace(v.Name), wrapper)
+						}
+					}
+				case *ast.BasicLit:
+					instance.ServiceName = strings.ReplaceAll(strings.TrimSpace(v.Value), "\"", "")
+				}
+
+				instances = append(instances, instance)
+			}
+		}
+	}
+
 	ast.Inspect(node, func(n ast.Node) bool {
 		if n == nil {
 			return false
@@ -22,61 +176,9 @@ func FindServiceDiscoveryWrappers(node ast.Node) []t.ServiceDiscoveryWrapper {
 
 		switch n := n.(type) {
 		case *ast.FuncDecl:
-			// Function declaration
-			wrapper = n.Name.Name
-			for _, param := range n.Type.Params.List {
-				for _, name := range param.Names {
-					paramNames = append(paramNames, name.Name)
-				}
-			}
-			// log.Printf("Parameter names: %v\n", paramNames)
-
+			handleFuncDecl(n)
 		case *ast.CallExpr:
-			// log.Printf("Call expression: %s\n", n.Fun)
-			if selExpr, ok := n.Fun.(*ast.SelectorExpr); ok {
-				// If the function is a list of nacos sdk functions
-				if util.Contains(select_sdk, selExpr.Sel.Name) {
-					// log.Printf("%s ", selExpr.Sel.Name)
-					for _, arg := range n.Args {
-
-						switch arg := arg.(type) {
-						case *ast.CompositeLit:
-							if sel, ok := arg.Type.(*ast.SelectorExpr); ok {
-
-								if util.Contains(select_params, sel.Sel.Name) {
-
-									instance := t.ServiceDiscoveryWrapper{}
-									instance.Wrapper = wrapper
-									for _, elt := range arg.Elts {
-										if kv, ok := elt.(*ast.KeyValueExpr); ok {
-											if key, ok := kv.Key.(*ast.Ident); ok {
-												if key.Name == "ServiceName" {
-													switch v := kv.Value.(type) {
-													case *ast.Ident:
-														for i, paramName := range paramNames {
-															if paramName == strings.TrimSpace(v.Name) {
-																instance.ServiceName = t.WrapperParams{Position: i}
-															}
-															if instance.ServiceName == nil {
-																instance.ServiceName = util.FindConstValue(node, strings.TrimSpace(v.Name), wrapper)
-															}
-														}
-													case *ast.BasicLit:
-														instance.ServiceName = strings.ReplaceAll(strings.TrimSpace(v.Value), "\"", "")
-													}
-												}
-											}
-										}
-									}
-
-									instances = append(instances, instance)
-
-								}
-							}
-						}
-					}
-				}
-			}
+			handleCallExpr(n)
 		}
 		return true
 	})
